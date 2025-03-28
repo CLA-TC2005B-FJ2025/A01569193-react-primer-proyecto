e4ec0e7fe62b32d71d06f947f48051b1ad373977
Santiago Estrada <152633890+santi-2000@users.noreply.github.com>
2025-03-28 16:01:20 +0000

    2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import { Tarjeta } from './Tarjeta'
+// importamos las imagenes 
+import MarioImage from'./Imagenes/Mario.png'
+import PeachImage from'./Imagenes/Peaches.png'
+import LuigiImage from'./Imagenes/Luigi.png'
 
 export function App() {
   return (
     <div>
         <div>Personajes de Mario Bros que mas me gistas</div>
-        <Tarjeta nombre= "Mario" colorfavorito= "rojo"/>
-        <Tarjeta nombre= "Luigi" colorfavorito= "Verde"/>
-        <Tarjeta nombre= "Peach" colorfavorito= "Rosa"/>
+        <Tarjeta nombre= "Mario" colorfavorito= "rojo" image = {MarioImage}/>
+        <Tarjeta nombre= "Luigi" colorfavorito= "Verde" image = {PeachImage}/>
+        <Tarjeta nombre= "Peach" colorfavorito= "Rosa" image = {LuigiImage}/>
     </div>
   )
 }
--- src/Tarjeta.js
+++ src/Tarjeta.js
@@ -3,6 +3,7 @@ import React from 'react'
 export function Tarjeta( props) {
   return (
     <div>
+      <img src = {props.image} width={150}/>
       <div> Nombre del personaje: {props.nombre}</div>
       <div> color favorito: {props.colorfavorito}</div>
         Tarjeta de Personaje de mario bross
